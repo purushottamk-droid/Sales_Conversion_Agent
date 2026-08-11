6b028a2f4a84dfc596d8c34eecbd3ab7356e3883
Gunjan <gunjan@atgeirsolutions.com>
2026-08-11 14:53:02 +0530

    Fix for email

--- auth/auth_setup.py
+++ auth/auth_setup.py
@@ -5,11 +5,7 @@ import os
 # =========================
 # CONFIG
 # =========================
-<<<<<<< HEAD
-CLIENT_SECRET_FILE = "client_secret_621913909275-2crq177heh5v7s83j8n260r4s3s15flr.apps.googleusercontent.com.json"
-=======
-CLIENT_SECRET_FILE = "client_secret_621913909275-gds5ve0u63kfuvhb35m03sn30pbkcina.apps.googleusercontent.com.json"
->>>>>>> a130be202adff6349c5d5beb2ed82a1c4474e182
+CLIENT_SECRET_FILE = "client_secret_621913909275-c0daeh86csg84hfq5f3l7uq6l6raap76.apps.googleusercontent.com.json"
 OUTPUT_FILE = "oauth_final.json"
 
 SCOPES = [
